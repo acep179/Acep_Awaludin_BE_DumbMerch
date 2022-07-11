5b8f27d1a4ac1a019f8a860b31948c255c330d0e
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-07-11 14:22:34 +0700

    Menambahkan controller dan router untuk addCategory, getCategories, getCategory, updateCategory, dan deleteCategory.

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,16 +5,24 @@ const router = express.Router();
 //. Controller
 const { addUsers, getUser} = require("../controllers/user");
 const { addProduct, getProducts, getProduct, updateProduct, deleteProduct } = require("../controllers/product");
+const { addCategory, getCategories, getCategory, updateCategory, deleteCategory } = require("../controllers/category");
 
 //. Router
 
 router.post("/register", addUsers)
 router.post("/login", getUser)
+
 router.post("/product", addProduct)
 router.get("/products", getProducts)
 router.get("/product/:id", getProduct)
 router.patch("/product/:id", updateProduct)
 router.delete("/product/:id", deleteProduct)
+
+router.post("/category", addCategory)
+router.get("/categories", getCategories)
+router.get("/category/:id", getCategory)
+router.patch("/category/:id", updateCategory)
+router.delete("/category/:id", deleteCategory)
 
 
 module.exports = router;
