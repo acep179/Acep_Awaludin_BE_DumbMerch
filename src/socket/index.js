fd4825942126ba00d16f61273fd9d6e976218f4e
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-07-27 13:53:05 +0700

    Menambahkan event message untuk menampilkan pesan pada halaman Compalain Admin dan Customer menggunakan socket.io

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,9 +1,15 @@
 const { user, profile, chat } = require("../../models")
 
+const { Op } = require('sequelize')
+const jwt = require('jsonwebtoken')
+
+const connectedUser = {}
+
 const socketIo = (io) => {
 
+
     io.use((socket, next) => {
-        if (socket.handshake.auth && socket.handshake.auth.token ) {
+        if (socket.handshake.auth && socket.handshake.auth.token) {
             next();
         } else {
             next(new Error("Not Authorized"));
@@ -12,6 +18,10 @@ const socketIo = (io) => {
 
     io.on('connection', (socket) => {
         console.log('client connect:', socket.id);
+
+        const userId = socket.handshake.query.id
+
+        connectedUser[userId] = socket.id
 
         socket.on("load admin contact", async () => {
             try {
@@ -80,9 +90,82 @@ const socketIo = (io) => {
             }
         })
 
+        socket.on("load messages", async (payload) => {
+            try {
+                const token = socket.handshake.auth.token
+                const tokenKey = process.env.TOKEN_KEY
+
+                const verified = jwt.verify(token, tokenKey)
+
+                const idRecipient = payload // catch recipient id sent from client
+                const idSender = verified.id //id user
+
+                const data = await chat.findAll({
+                    where: {
+                        idSender: {
+                            [Op.or]: [idRecipient, idSender]
+                        },
+                        idRecipient: {
+                            [Op.or]: [idRecipient, idSender]
+                        }
+                    },
+                    include: [
+                        {
+                            model: user,
+                            as: "recipient",
+                            attributes: {
+                                exclude: ["createdAt", "updatedAt", "password"],
+                            },
+                        },
+                        {
+                            model: user,
+                            as: "sender",
+                            attributes: {
+                                exclude: ["createdAt", "updatedAt", "password"],
+                            },
+                        },
+                    ],
+                    order: [['createdAt', 'ASC']],
+                    attributes: {
+                        exclude: ["createdAt", "updatedAt", "idRecipient", "idSender"],
+                    }
+                })
+
+                socket.emit("messages", data)
+            } catch (error) {
+                console.log(error)
+            }
+        })
+
+        socket.on("send messages", async (payload) => {
+
+            try {
+                const token = socket.handshake.auth.token
+
+                const tokenKey = process.env.TOKEN_KEY
+                const verified = jwt.verify(token, tokenKey)
+
+                const idSender = verified.id //id user
+                const { message, idRecipient } = payload
+
+                await chat.create({
+                    message,
+                    idRecipient,
+                    idSender
+                })
+
+
+                io.to(socket.id).to(connectedUser[idRecipient]).emit("new message", idRecipient)
+            } catch (error) {
+                console.log(error);
+            }
+        })
+
 
         socket.on('disconnect', () => {
             console.log('client disconnect');
+
+            delete connectedUser[userId]
         })
     })
 }
